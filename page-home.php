08a6013f51d5b62c187667b4a577b6e76ded5144
tigerstripemedia <info@tigerstripemedia.co.uk>
2018-08-09 14:20:52 +0000

    Services Pages Custom Fields

--- page-home.php
+++ page-home.php
@@ -192,7 +192,7 @@ get_header();
       </div>
     </section>
     
-    <?php echo do_shortcode('[instagram-feed]'); ?>
+    <?php echo do_shortcode('[instagram-feed user="tigerstripemedia"]'); ?>
 
     <!--<section id="testimonials">-->
     <!--  <div class="container">-->
