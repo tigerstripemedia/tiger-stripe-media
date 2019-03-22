e5b9527aa259787d170286f6c6f414c9cff76b18
tigerstripemedia <info@tigerstripemedia.co.uk>
2019-03-22 10:32:28 +0000

    JS Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,3 @@
-// // For the typewriter text animation on home screen
-// var TxtType = function(el, toRotate, period) {
-//         this.toRotate = toRotate;
-//         this.el = el;
-//         this.loopNum = 0;
-//         this.period = parseInt(period, 10) || 2000;
-//         this.txt = '';
-//         this.tick();
-//         this.isDeleting = false;
-//     };
-
-// TxtType.prototype.tick = function() {
-//     var i = this.loopNum % this.toRotate.length;
-//     var fullTxt = this.toRotate[i];
-
-//     if (this.isDeleting) {
-//     this.txt = fullTxt.substring(0, this.txt.length - 1);
-//     } else {
-//     this.txt = fullTxt.substring(0, this.txt.length + 1);
-//     }
-
-//     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-//     var that = this;
-//     var delta = 200 - Math.random() * 100;
-
-//     if (this.isDeleting) { delta /= 2; }
-
-//     if (!this.isDeleting && this.txt === fullTxt) {
-//     delta = this.period;
-//     this.isDeleting = true;
-//     } else if (this.isDeleting && this.txt === '') {
-//     this.isDeleting = false;
-//     this.loopNum++;
-//     delta = 500;
-//     }
-
-//     setTimeout(function() {
-//     that.tick();
-//     }, delta);
-// };
-
-// window.onload = function() {
-//     var elements = document.getElementsByClassName('typewrite');
-//     for (var i=0; i<elements.length; i++) {
-//         var toRotate = elements[i].getAttribute('data-type');
-//         var period = elements[i].getAttribute('data-period');
-//         if (toRotate) {
-//           new TxtType(elements[i], JSON.parse(toRotate), period);
-//         }
-//     }
-//     // INJECT CSS
-//     var css = document.createElement("style");
-//     css.type = "text/css";
-//     css.innerHTML = ".typewrite > .wrap { border-right: 0.06em solid #f79447}";
-//     document.body.appendChild(css);
-// };
-
 // New TypeWriter
 document.addEventListener('DOMContentLoaded',function(event){
   
@@ -122,16 +64,18 @@ $(".navbar-second").find("a").click(function(e) {
     }, 2000);
 });
 
-// Fix second nav top on scroll
-window.onscroll = function fixedScroll() {secNavScroll()};
-
-var header = document.getElementById("second-nav");
-var sticky = header.offsetTop - 63.22;
-
-function secNavScroll() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("fixed-top-second");
-  } else {
-    header.classList.remove("fixed-top-second");
+document.addEventListener('DOMContentLoaded', function() {
+  // Fix second nav top on scroll
+  window.onscroll = function fixedScroll() {secNavScroll()};
+  
+  var header = document.getElementById("second-nav");
+  var sticky = header.offsetTop - 63.22;
+  
+  function secNavScroll() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("fixed-top-second");
+    } else {
+      header.classList.remove("fixed-top-second");
+    }
   }
-}
+})
